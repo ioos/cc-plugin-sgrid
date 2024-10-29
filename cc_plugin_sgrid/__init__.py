d2fa1db8976fa7e11b25f04d6334272191d848da
Filipe Fernandes <ocefpaf@gmail.com>
2024-10-29 14:50:48 +0100

    use dynamic versioning and lint

--- cc_plugin_sgrid/__init__.py
+++ cc_plugin_sgrid/__init__.py
@@ -1,24 +1,26 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import os
 import logging
+import os
+
 from compliance_checker.base import BaseNCCheck, Result
 
-with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'VERSION')) as f:
-    __version__ = f.read().strip()
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "unknown"
+
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 
 class SgridChecker(BaseNCCheck):
-    _cc_spec = 'SGRID'
-    _cc_url = 'https://github.com/ioos/cc-plugin-sgrid'
-    _cc_author = 'Kyle Wilcox <kyle@axiomdatascience.com>'
+    _cc_spec = "SGRID"
+    _cc_url = "https://github.com/ioos/cc-plugin-sgrid"
+    _cc_author = "Kyle Wilcox <kyle@axiomdatascience.com>"
     _cc_checker_version = __version__
 
     @classmethod
-    def beliefs(cls): 
+    def beliefs(cls):
         return {}
 
     @classmethod
