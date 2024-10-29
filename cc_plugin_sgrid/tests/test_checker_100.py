ff8f9669905fe8696040ba951f2585fb167d9063
Filipe Fernandes <ocefpaf@gmail.com>
2024-10-29 14:55:43 +0100

    use pytest and improve ncfile loading

--- cc_plugin_sgrid/tests/test_checker_100.py
+++ cc_plugin_sgrid/tests/test_checker_100.py
@@ -1,138 +1,158 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import os
-import shutil
 import logging
-import unittest
-import tempfile
-from pkg_resources import resource_filename as rs
+from pathlib import Path
 
-import netCDF4 as nc4
+import pytest
+from netCDF4 import Dataset
 
-from cc_plugin_sgrid.checker_100 import SgridChecker100
 from cc_plugin_sgrid import logger
+from cc_plugin_sgrid.checker_100 import SgridChecker100
+
 logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.DEBUG)
 
 
-class TestSgridChecker100(unittest.TestCase):
-
-    def setUp(self):
-        self.check = SgridChecker100()
-
-    def nc(self, ncpath):
-        _, tf = tempfile.mkstemp(suffix='.nc')
-        shutil.copy(ncpath, tf)
-        ncd = nc4.Dataset(tf, 'r+')
-        self.addCleanup(ncd.close)
-        self.addCleanup(lambda: os.remove(tf))
-        return ncd
-
-    def test_check_grid_variable(self):
-        ncd = self.nc(rs('cc_plugin_sgrid', os.path.join('resources', 'roms.nc')))
-
-        r = self.check.check_grid_variable(ncd)
-        assert r.value == (1, 1)
-
-    def test_check_invalid_grid_variable(self):
-        ncd = self.nc(rs('cc_plugin_sgrid', os.path.join('resources', 'roms.nc')))
-        ncd.variables['grid'].cf_role = 'blah'
-        r = self.check.check_grid_variable(ncd)
-        assert r.value == (0, 1)
-
-    def test_check_mesh_toplogy_grid_variable(self):
-        ncd = self.nc(rs('cc_plugin_sgrid', os.path.join('resources', 'roms.nc')))
-        ncd.variables['grid'].cf_role = 'mesh_toplogy'
-        r = self.check.check_grid_variable(ncd)
-        assert r.value == (0, 1)
+romsnc = (
+    Path(__file__).absolute().parent.parent.joinpath("resources", "roms.nc")
+)
 
 
-    def test_check_no_cf_role_variable(self):
-        ncd = self.nc(rs('cc_plugin_sgrid', os.path.join('resources', 'roms.nc')))
-        del ncd.variables['grid'].cf_role
-        r = self.check.check_grid_variable(ncd)
-        assert r.value == (0, 1)        
+@pytest.fixture
+def check():
+    """Load checker."""
+    return SgridChecker100()
 
-    def test_check_topology_dimension(self):
-        ncd = self.nc(rs('cc_plugin_sgrid', os.path.join('resources', 'roms.nc')))
 
-        r = self.check.check_topology_dimension(ncd)
-        assert r.value == (1, 1)
+@pytest.fixture
+def nc():
+    """Load test data in memory."""
+    dset = Dataset(
+        romsnc,
+        "r+",
+        diskless=True,
+        persist=False,
+    )
+    yield dset
+    dset.close()
 
-        ncd.variables['grid'].topology_dimension = 0
-        r = self.check.check_topology_dimension(ncd)
-        assert r.value == (0, 1)
 
-        ncd.variables['grid'].topology_dimension = 9
-        r = self.check.check_topology_dimension(ncd)
-        assert r.value == (0, 1)
+def test_check_grid_variable(check, nc):
+    """Test check_grid_variable."""
+    r = check.check_grid_variable(nc)
+    assert r.value == (1, 1)
 
-    def test_check_node_dimension_size(self):
-        ncd = self.nc(rs('cc_plugin_sgrid', os.path.join('resources', 'roms.nc')))
 
-        r = self.check.check_node_dimensions_size(ncd)
-        assert r.value == (1, 1)
+def test_check_invalid_grid_variable(check, nc):
+    """Test check_invalid_grid_variable."""
+    nc.variables["grid"].cf_role = "blah"
+    r = check.check_grid_variable(nc)
+    assert r.value == (0, 1)
 
-        ncd.variables['grid'].topology_dimension = 3
-        r = self.check.check_node_dimensions_size(ncd)
-        assert r.value == (0, 1)
 
-        ncd.variables['grid'].topology_dimension = 2
-        ncd.variables['grid'].node_dimensions = 'first second third'
-        r = self.check.check_node_dimensions_size(ncd)
-        assert r.value == (0, 1)
+def test_check_mesh_toplogy_grid_variable(check, nc):
+    """Test check_mesh_toplogy_grid_variable."""
+    nc.variables["grid"].cf_role = "mesh_toplogy"
+    r = check.check_grid_variable(nc)
+    assert r.value == (0, 1)
 
-    def test_check_node_dimension_dimensions(self):
-        ncd = self.nc(rs('cc_plugin_sgrid', os.path.join('resources', 'roms.nc')))
 
-        r = self.check.check_node_dimensions_dimensions(ncd)
-        assert r.value == (1, 1)
+def test_check_no_cf_role_variable(check, nc):
+    """Test check_no_cf_role_variable."""
+    del nc.variables["grid"].cf_role
+    r = check.check_grid_variable(nc)
+    assert r.value == (0, 1)
 
-        ncd.variables['grid'].node_dimensions = 'hi bye'
-        r = self.check.check_node_dimensions_dimensions(ncd)
-        assert r.value == (0, 1)
 
-    def test_check_face_dimension_size(self):
-        ncd = self.nc(rs('cc_plugin_sgrid', os.path.join('resources', 'roms.nc')))
+def test_check_topology_dimension(check, nc):
+    """Test check_topology_dimension."""
+    r = check.check_topology_dimension(nc)
+    assert r.value == (1, 1)
 
-        r = self.check.check_face_dimensions_size(ncd)
-        assert r.value == (1, 1)
+    nc.variables["grid"].topology_dimension = 0
+    r = check.check_topology_dimension(nc)
+    assert r.value == (0, 1)
 
-        ncd.variables['grid'].topology_dimension = 3
-        r = self.check.check_face_dimensions_size(ncd)
-        assert r.value == (0, 1)
+    nc.variables["grid"].topology_dimension = 9
+    r = check.check_topology_dimension(nc)
+    assert r.value == (0, 1)
 
-        ncd.variables['grid'].topology_dimension = 2
-        ncd.variables['grid'].face_dimensions = 'first second third'
-        r = self.check.check_face_dimensions_size(ncd)
-        assert r.value == (0, 1)
 
-        ncd.variables['grid'].face_dimensions = 'hi: bye (padding: foo)'
-        r = self.check.check_face_dimensions_size(ncd)
-        assert r.value == (0, 1)
+def test_check_node_dimension_size(check, nc):
+    """Test check_node_dimension_size."""
+    r = check.check_node_dimensions_size(nc)
+    assert r.value == (1, 1)
 
-    def test_check_face_dimension_dimensions(self):
-        ncd = self.nc(rs('cc_plugin_sgrid', os.path.join('resources', 'roms.nc')))
+    nc.variables["grid"].topology_dimension = 3
+    r = check.check_node_dimensions_size(nc)
+    assert r.value == (0, 1)
 
-        r = self.check.check_face_dimensions_dimensions(ncd)
-        assert r.value == (1, 1)
+    nc.variables["grid"].topology_dimension = 2
+    nc.variables["grid"].node_dimensions = "first second third"
+    r = check.check_node_dimensions_size(nc)
+    assert r.value == (0, 1)
 
-        ncd.variables['grid'].face_dimensions = 'hi: bye (padding: foo)'
-        r = self.check.check_face_dimensions_dimensions(ncd)
-        assert r is None  # Size doesn't match, which is a dependency
 
-        ncd.variables['grid'].face_dimensions = 'xi_rho: xi_psi (padding: NOTCORRECT) eta_rho: eta_psi (padding: both)'
-        r = self.check.check_face_dimensions_dimensions(ncd)
-        assert r.value == (0, 1)
+def test_check_node_dimension_dimensions(check, nc):
+    """Test check_node_dimension_dimensions."""
+    r = check.check_node_dimensions_dimensions(nc)
+    assert r.value == (1, 1)
 
-        ncd.variables['grid'].face_dimensions = 'xi_rho: xi_psi (NOTPADDING: both) eta_rho: eta_psi (padding: both)'
-        r = self.check.check_face_dimensions_dimensions(ncd)
-        assert r.value == (0, 1)
+    nc.variables["grid"].node_dimensions = "hi bye"
+    r = check.check_node_dimensions_dimensions(nc)
+    assert r.value == (0, 1)
 
-        ncd.variables['grid'].face_dimensions = 'xi_rho: xi_psi (padding: low) eta_rho: eta_psi (padding: high)'
-        r = self.check.check_face_dimensions_dimensions(ncd)
-        assert r.value == (1, 1)
 
-        #ncd.variables['grid'].face_dimensions = 'hi: bye (padding: foo)'
-        #r = self.check.check_face_dimensions_dimensions(ncd)
-        #assert r is None  # Size doesn't match, which is a dependency
+def test_check_face_dimension_size(check, nc):
+    """Test check_face_dimension_size."""
+    r = check.check_face_dimensions_size(nc)
+    assert r.value == (1, 1)
+
+    nc.variables["grid"].topology_dimension = 3
+    r = check.check_face_dimensions_size(nc)
+    assert r.value == (0, 1)
+
+    nc.variables["grid"].topology_dimension = 2
+    nc.variables["grid"].face_dimensions = "first second third"
+    r = check.check_face_dimensions_size(nc)
+    assert r.value == (0, 1)
+
+    nc.variables["grid"].face_dimensions = "hi: bye (padding: foo)"
+    r = check.check_face_dimensions_size(nc)
+    assert r.value == (0, 1)
+
+
+def test_check_face_dimension_dimensions(check, nc):
+    """Test check_face_dimension_dimensions."""
+    r = check.check_face_dimensions_dimensions(nc)
+    assert r.value == (1, 1)
+
+    nc.variables["grid"].face_dimensions = "hi: bye (padding: foo)"
+    r = check.check_face_dimensions_dimensions(nc)
+    assert r is None  # Size doesn't match, which is a dependency
+
+    nc.variables[
+        "grid"
+    ].face_dimensions = (
+        "xi_rho: xi_psi (padding: NOTCORRECT) eta_rho: eta_psi (padding: both)"
+    )
+    r = check.check_face_dimensions_dimensions(nc)
+    assert r.value == (0, 1)
+
+    nc.variables[
+        "grid"
+    ].face_dimensions = (
+        "xi_rho: xi_psi (NOTPADDING: both) eta_rho: eta_psi (padding: both)"
+    )
+    r = check.check_face_dimensions_dimensions(nc)
+    assert r.value == (0, 1)
+
+    nc.variables[
+        "grid"
+    ].face_dimensions = (
+        "xi_rho: xi_psi (padding: low) eta_rho: eta_psi (padding: high)"
+    )
+    r = check.check_face_dimensions_dimensions(nc)
+    assert r.value == (1, 1)
+
+    # nc.variables['grid'].face_dimensions = 'hi: bye (padding: foo)'
+    # r = check.check_face_dimensions_dimensions(nc)
+    # assert r is None  # Size doesn't match, which is a dependency
